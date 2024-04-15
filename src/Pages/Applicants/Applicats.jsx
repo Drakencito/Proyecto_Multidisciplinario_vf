0bf2ee0aee56f1e14657fb7a96f6038f86cc5e42
9edroPablo <pedropablopep@gmail.com>
2024-04-14 18:02:32 -0600

    Actualizacion 14/04/24 Solicitudes-Botones

--- src/Pages/Applicants/Applicats.jsx
+++ src/Pages/Applicants/Applicats.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import will from '../../assets/will.jpg'
+import eliminar from '../../assets/borrar.png'
+import editar from '../../assets/editar.png'
 import '../Applicants/Applicants.css'
 function ApplicantsX (props){
     return(
@@ -15,6 +17,12 @@ function ApplicantsX (props){
             <div className="info">
                 <p>Numero: {props.tell}</p>
                 <p className="correo">Correo: {props.mail}</p>
+            </div>
+
+            <div className="boxBotonAplicants">
+                <button className="botonAplicants"><img className="botonAplicants" src={eliminar}  /></button>
+                <button className="botonAplicants"><img className="botonAplicants" src={editar}  /></button>
+                
             </div>
         </div>
     )
